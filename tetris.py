51f51246e6637766c5fe6bc07210d8c2befb556d
Jake the Bake <62112943+jacobfernstrom@users.noreply.github.com>
2022-11-26 09:35:19 +0100

    2nd

--- tetris.py
+++ tetris.py
@@ -1,13 +1,7 @@
 from random import randint
 import os
 from pytimedinput import timedInput
-#TODO
-# 1. rita upp en spelplan
-# 2. definiera block, 5 funktioner? block, rad, zickzack, kant, alla block har 4x#
-# 3. listor, aktivt block, kommande_block, block som nått botten,
-# 4. 
-#
-#
+
 
 
 
@@ -24,6 +18,9 @@ def draw_board ():
         
         elif pos in new_block:
             print('#', end = '')
+        
+        elif pos in bottom_blocks:
+            print('#', end = '')
        
         else:
             print(' ', end = '')
@@ -36,12 +33,10 @@ def draw_board ():
 def draw_tetroid (): # assign start position to tetroid
     if len(new_block) == 0:   
         temp_var = randint(0,6)
-        
-
         if temp_var == 0: #start position of O-block
             for i in range(2):
-                new_block.append((start_pos_block[0]+1,start_pos_block[1] + i))
                 new_block.append((start_pos_block[0],start_pos_block[1] + i))
+                new_block.append((start_pos_block[0]+1,start_pos_block[1] + i))    
                 
         elif temp_var == 1: # starting position of I-block
             for i in range(4):
@@ -73,21 +68,51 @@ def draw_tetroid (): # assign start position to tetroid
         
             new_block.append((start_pos_block[0] + 1,start_pos_block[1] + 2))
 
-        
 
-def update_movement ():
-    active_block = new_block
+     
+    
     print
 
 def update_tetroid ():
-    for i in range(len(new_block)):
-        new_block[i] = (new_block[i][0] + direction[0], new_block[i][1] + direction[1])
-    for i in range(len(new_block)):
-        new_block[i] = (new_block[i][0]+1, new_block[i][1])
+    count = 0
+    count_two = 0
     
-def check_bottom():
-    print
+    for part in new_block:
+        if part[1] == grid_width - 2:
+            count +=1
+        elif part[1]== 1:
+            count_two += 1
+    
+    
+    for i in range(len(new_block)):
+        if count > 0 and direction == (0, -1):
+            new_block[i] = (new_block[i][0]+direction[0], new_block[i][1] + direction[1])
+        elif count_two > 0 and direction == (0, 1):
+            new_block[i] = (new_block[i][0]+direction[0], new_block[i][1] + direction[1])
+        elif count == 0 and count_two == 0:
+            new_block[i] = (new_block[i][0]+direction[0], new_block[i][1] + direction[1])
+        
+        
+   
+        
 
+    
+            
+
+    for i in range(len(new_block)): # pushe tetroid down one step
+        new_block[i] = (new_block[i][0]+1, new_block[i][1])
+
+def check_bottom():
+    global count_bottom
+    count_bottom = 0
+    for part in new_block:
+        part_temp = (part[0] + 1, part[1])
+        if (part_temp in bottom_blocks) or part_temp[0] == grid_height-1:
+            count_bottom =1
+    if count_bottom == 1:
+        bottom_blocks.extend(new_block)
+        new_block.clear()
+    
 
 #functions/methods
 
@@ -97,17 +122,19 @@ def check_bottom():
 
 
 #declarables 
-
 grid_height = 18
 grid_width = 12
-start_pos_block = (0,grid_width/2-1)
-new_block = []
-active_block = []
-end_block = []
 
+#LISTS
+start_pos_block = (0,grid_width/2)
+new_block = []
+bottom_blocks = []
+
+#directional data
 DIRECTION ={'left':(0,-1), 'right':(0,1),'up':(0,0), 'end_game':(0,0), 'down':(1,0)} 
 direction = DIRECTION['up'] 
 
+#col/row loop
 game_board = [(col,row) for col in range(grid_height) for row in range(grid_width)]
 
 
@@ -115,9 +142,9 @@ game_board = [(col,row) for col in range(grid_height) for row in range(grid_widt
 #MAIN_loop
 while True:
     draw_tetroid()
-    print(new_block)
+    
     draw_board()
-    txt,_ = timedInput(timeout = 0.7)
+    txt,_ = timedInput(timeout = 0.5)
     match txt:
         case 'w': direction = DIRECTION['up']
         case 'a': direction = DIRECTION['left']
@@ -125,4 +152,5 @@ while True:
         case '': direction = DIRECTION['end_game']
         case ' ': break
     update_tetroid()
+    check_bottom()
     
